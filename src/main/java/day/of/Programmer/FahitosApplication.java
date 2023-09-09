375f7207003cd4bd5eac12e5594e3bb88a18ecb0
Straga07 <Straga07@yandex.ru>
2023-09-09 15:23:51 +0300

    feat: init

---
+++ src/main/java/day/of/Programmer/FahitosApplication.java
@@ -0,0 +1,13 @@
+package day.of.Programmer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FahitosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FahitosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/day/of/Programmer/FahitosApplicationTests.java
@@ -0,0 +1,13 @@
+package day.of.Programmer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FahitosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

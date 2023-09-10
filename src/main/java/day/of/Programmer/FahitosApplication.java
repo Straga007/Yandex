c4fcb7b4dd368b1296d99e499fa87abec4b45c19
Straga07 <Straga07@yandex.ru>
2023-09-10 21:35:08 +0300

    feat: decoder

--- src/main/java/day/of/Programmer/FahitosApplication.java
+++ src/main/java/day/of/Programmer/FahitosApplication.java
@@ -1,48 +1,105 @@
 package day.of.Programmer;
 
-import org.apache.http.HttpEntity;
-import org.apache.http.client.methods.CloseableHttpResponse;
-import org.apache.http.client.methods.HttpPost;
-import org.apache.http.entity.ContentType;
-import org.apache.http.entity.StringEntity;
-import org.apache.http.impl.client.CloseableHttpClient;
-import org.apache.http.impl.client.HttpClients;
-import org.apache.http.util.EntityUtils;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 public class FahitosApplication {
 
     public static void main(String[] args) {
-        String url = "http://ya.praktikum.fvds.ru:8080/dev-day/task/2";
-
-        String encodedText = "BUPY U JYLZYWN JBJ WIXCHA XUS";
-        int offset = 20;
-
-        String decodedText = decode(encodedText, offset);
-
-        String json = "{\"decoded\": \"" + decodedText + "\"}";
+        String apiUrl = "http://ya.praktikum.fvds.ru:8080/dev-day/task/2";
+        String authToken = "f538ec47-514d-4051-8be9-0e91bf174b6e";
 
         try {
-            CloseableHttpClient httpClient = HttpClients.createDefault();
-            HttpPost httpPost = new HttpPost(url);
+            URL url = new URL(apiUrl);
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            connection.setRequestMethod("GET");
 
-            // Добавляем заголовок AUTH_TOKEN
-            httpPost.setHeader("AUTH_TOKEN", "f538ec47-514d-4051-8be9-0e91bf174b6e");
+            connection.setRequestProperty("AUTH_TOKEN", authToken);
 
-            StringEntity entity = new StringEntity(json, ContentType.APPLICATION_JSON);
-            httpPost.setEntity(entity);
+            int responseCode = connection.getResponseCode();
 
-            CloseableHttpResponse response = httpClient.execute(httpPost);
+            if (responseCode == HttpURLConnection.HTTP_OK) {
+                BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+                String line;
+                StringBuilder encoded = new StringBuilder();
+                StringBuilder offset = new StringBuilder();
+                StringBuilder numb = new StringBuilder();
 
-            HttpEntity responseEntity = response.getEntity();
-            String responseBody = EntityUtils.toString(responseEntity);
+                while ((line = reader.readLine()) != null) {
+                    if (line.contains("encoded") && line.contains("offset")) {
+                        encoded.append(line);
+                        encoded = new StringBuilder(encoded.substring(57));
+                        for (int i = 0; i < encoded.length(); i++) {
+                            if (encoded.charAt(i) != '&') {
+                                offset.append(encoded.charAt(i));
+                            } else {
+                                break;
+                            }
+                        }
+                        for (int i = 0; i < encoded.length(); i++) {
+                            char ch = encoded.charAt(i);
+                            if (Character.isDigit(ch)) {
+                                numb.append(ch);
+                            }
+                        }
+                    }
+                }
+                Integer num = Integer.parseInt(numb.toString());
+                reader.close();
 
-            System.out.println("Ответ сервера:");
-            System.out.println(responseBody);
+                System.out.println(offset);
+                System.out.println(num);
 
-            response.close();
-            httpClient.close();
+                String decodedText = decode(offset.toString(), num);
 
-        } catch (Exception e) {
+                sendPostRequest(apiUrl, authToken, decodedText);
+
+            } else {
+                System.out.println("Запрос не удался. Код ответа: " + responseCode);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static void sendPostRequest(String apiUrl, String authToken, String decodedText) {
+        try {
+            URL url = new URL(apiUrl);
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            connection.setRequestMethod("POST");
+
+            connection.setRequestProperty("AUTH_TOKEN", authToken);
+            connection.setRequestProperty("Content-Type", "application/json");
+
+            connection.setDoOutput(true);
+            OutputStream os = connection.getOutputStream();
+            byte[] input = String.format("{\"decoded\": \"%s\"}", decodedText).getBytes("utf-8");
+            os.write(input, 0, input.length);
+
+            int responseCode = connection.getResponseCode();
+
+            if (responseCode == HttpURLConnection.HTTP_OK) {
+                BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+                String line;
+                StringBuilder response = new StringBuilder();
+
+                while ((line = reader.readLine()) != null) {
+                    response.append(line);
+                }
+                reader.close();
+
+                System.out.println("Ответ сервера:");
+                System.out.println(response.toString());
+            } else {
+                System.out.println("POST-запрос не удался. Код ответа: " + responseCode);
+            }
+
+            os.close();
+        } catch (IOException e) {
             e.printStackTrace();
         }
     }
